25ab5d2d66073466db1ffcd2617c775f35fb1574
danbond85@mail.ru <danbond85@gmail.com>
2018-04-15 19:51:19 +0300

    DOM: Shop minor changes

--- dom/shop/js/app.js
+++ dom/shop/js/app.js
@@ -2,7 +2,7 @@ const buttons= document.querySelectorAll('button.add');
 const cartCount = document.querySelector('#cart-count');
 const cartPrice = document.querySelector('span#cart-total-price');
 
-Array.from(buttons).forEach(button =>{
+Array.from(buttons).forEach(button => {
   button.addEventListener('click', () => {
     cartCount.innerHTML = parseInt(cartCount.innerHTML) + 1;
     cartPrice.innerHTML = parseInt(cartPrice.innerHTML)
