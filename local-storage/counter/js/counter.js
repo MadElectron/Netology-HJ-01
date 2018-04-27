99b757f17afd7f30e155e9817f4a8aab26f6c56e
danbond85@mail.ru <danbond85@gmail.com>
2018-04-27 23:43:15 +0300

    Local storage: counter released

--- local-storage/counter/js/counter.js
+++ local-storage/counter/js/counter.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const counter = document.getElementById('counter');
+const inc = document.getElementById('increment');
+const dec = document.getElementById('decrement');
+const reset = document.getElementById('reset');
+
+if (localStorage.counter === undefined) {
+  localStorage.setItem('counter', 0);
+}
+counter.textContent = localStorage.counter;
+
+inc.addEventListener('click', event => {
+  localStorage.setItem('counter', ++localStorage.counter);
+  counter.textContent = localStorage.counter;
+});
+
+dec.addEventListener('click', event => {
+  localStorage.setItem('counter', --localStorage.counter);
+  counter.textContent = localStorage.counter;
+});
+
+reset.addEventListener('click', event => {
+  localStorage.setItem('counter', 0);
+  counter.textContent = localStorage.counter;
+});
