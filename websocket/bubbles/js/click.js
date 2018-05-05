d6b7bd753888901254aabbcc14c1cf6d5eec67ee
danbond85@mail.ru <danbond85@gmail.com>
2018-05-05 20:17:26 +0300

    Websocket: bubbles released

--- websocket/bubbles/js/click.js
+++ websocket/bubbles/js/click.js
@@ -1,1 +1,13 @@
-'use strinct';
+'use strict';
+
+const canvas = document.querySelector('canvas');
+const conn = new WebSocket('wss://neto-api.herokuapp.com/mouse');
+
+showBubbles(conn);
+
+canvas.addEventListener('click', event => {
+  conn.send(JSON.stringify({
+    'x' : event.pageX,
+    'y' : event.pageY,
+  }));
+})
