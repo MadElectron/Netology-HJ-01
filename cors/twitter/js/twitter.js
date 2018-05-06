503e339a44282ccd7597fbbb959a82ff5c6a71ad
danbond85@mail.ru <danbond85@gmail.com>
2018-05-06 16:27:33 +0300

    Cors: twitter released

--- cors/twitter/js/twitter.js
+++ cors/twitter/js/twitter.js
@@ -1,1 +1,25 @@
 'use strict';
+
+function appendData(data) {
+  Object.entries(data).forEach((prop) => {
+    const [key, value] = prop;
+    const node = document.querySelector(`*[data-${key}]`);
+    
+    if (node.tagName === 'IMG') {
+      node.src = value;
+    } else {
+      node.textContent = value;
+    }
+  });
+}
+
+function loadData() {
+  return new Promise((done, fail) => {
+    const script = document.createElement('script');
+    script.src = 'https://neto-api.herokuapp.com/twitter/jsonp?callback=appendData';
+    document.body.appendChild(script);
+  });
+}
+
+
+loadData().then(appendData);
