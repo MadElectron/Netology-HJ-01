37978a53097a8a25a343290f4ae42cc5a169986d
danbond85@mail.ru <danbond85@gmail.com>
2018-05-06 17:28:59 +0300

    Cors: profile released without Promises

--- cors/profile/js/profile.js
+++ cors/profile/js/profile.js
@@ -1,1 +1,54 @@
 'use strict';
+
+const content = document.querySelector('.content');
+
+function loadData(url, callback) {
+  return new Promise((done, fail) => {
+    const script = document.createElement('script');
+    script.src = `${url}?callback=${callback}`;
+    document.body.appendChild(script);
+  });
+}
+
+
+loadData('https://neto-api.herokuapp.com/profile/me', 'appendProfileData')
+  .then(appendProfileData);
+  
+
+function appendProfileData(data) {
+  console.log(data)
+
+  Object.entries(data).forEach((prop) => {
+
+    const [key, value] = prop;
+    const node = document.querySelector(`*[data-${key}]`);
+    
+    if (node) {
+      if (node.tagName === 'IMG') {
+        node.src = value;
+      } else {
+        node.textContent = value;
+      }      
+    }
+  });
+
+  loadData(`https://neto-api.herokuapp.com/profile/${data.id}/technologies`, 'appendTechnologiesData')
+  .then(appendTechnologiesData);
+}
+
+function appendTechnologiesData(data) {
+  console.log(data);
+
+  const node = document.querySelector(`*[data-technologies]`);
+  data.forEach(el => {
+    const span = document.createElement('span');
+    span.classList = `devicons devicons-${el}`;
+    node.appendChild(span);
+  })  
+
+  displayContent();
+}
+
+function displayContent() {
+  content.style.display = 'initial';
+}
