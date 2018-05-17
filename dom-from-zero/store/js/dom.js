09a9d40863e977594bdf96458d667c561601d8bf
danbond85@mail.ru <danbond85@gmail.com>
2018-05-17 23:24:25 +0300

    DFZ store released

--- dom-from-zero/store/js/dom.js
+++ dom-from-zero/store/js/dom.js
@@ -1,1 +1,26 @@
 'use strict';
+
+function createElement(node) {
+  let el;
+
+  if (typeof node === 'object') {
+    el = document.createElement(node.name);
+    let childNode;
+
+    if (node.props != null) {
+      Object.entries(node.props).forEach((v,k) => {
+        el.className = v[1];
+        console.log(v);
+      });
+    }
+
+    node.childs.forEach(child => {
+      childNode = createElement(child);
+      el.appendChild(childNode);
+    });
+  } else {
+    el = document.createTextNode(node);
+  }
+
+  return el;
+}
