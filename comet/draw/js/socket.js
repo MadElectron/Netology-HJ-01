9de089dc0938ccce2315403a7bd31aa3a33e94a1
danbond85@mail.ru <danbond85@gmail.com>
2018-05-19 01:20:43 +0300

    Random released

--- comet/draw/js/socket.js
+++ comet/draw/js/socket.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const conn = new WebSocket('wss://neto-api.herokuapp.com/draw');
+
+conn.addEventListener('open', () => {
+  console.log('Вебсокет-соединение открыто');
+});
+
+conn.addEventListener('message', evt => {
+  console.log(`Ответ: ${evt.data}`);
+});
+
+window.editor.addEventListener('update', evt => {
+  conn.send(evt.canvas);
+});
+
--- comet/random/js/websocket.js
+++ comet/random/js/websocket.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const poolingUrl = 'https://neto-api.herokuapp.com/comet/pooling';
+const longPoolingUrl = 'https://neto-api.herokuapp.com/comet/long-pooling';
+const interval = 5000;
+const longInterval = 7000;
+
+const conn = new WebSocket('wss://neto-api.herokuapp.com/comet/websocket');
+
+function setNumber(num, connType) {
+  const cards = document.querySelectorAll(`section.${connType} > div`);
+  const card = [...cards].find(el => el.textContent === num);
+  // console.log(card);
+
+  [...cards].forEach(el => el.classList.remove('flip-it'));
+  card.classList.add('flip-it');
+}
+
+
+setInterval(() => {
+  fetch(poolingUrl)
+    .then(resp => resp.text())
+    .then(data => setNumber(data, 'pooling'));
+}, interval);
+
+setInterval(() => {
+  fetch(longPoolingUrl)
+    .then(resp => resp.text())
+    .then(data => setNumber(data.trim(), 'long-pooling'));
+}, longInterval);
+
+conn.addEventListener('message', evt => {
+  setNumber(evt.data, 'websocket');
+});
