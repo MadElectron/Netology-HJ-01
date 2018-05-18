d51fb3414d4134e0e6b4be823e448b3b7298b2f5
danbond85@mail.ru <danbond85@gmail.com>
2018-05-18 23:44:06 +0300

    Realtime released

--- comet/realtime/js/realtime.js
+++ comet/realtime/js/realtime.js
@@ -14,15 +14,16 @@ const realtime = new Chart(ctx).Bar({
 let isFirst = true;
 const ws = new WebSocket('wss://neto-api.herokuapp.com/realtime');
 ws.addEventListener('message', event => {
+  const jsonData = JSON.parse(event.data);
+
   if (isFirst) {
-    event.data
-      .split('\n')
-      .map(line => line.split('|'))
-      .forEach(data => realtime.addData([Number(data[1])], data[0]));
+    jsonData.forEach(data => 
+      realtime.addData([Number(data.online)], data.time)
+    );
 
     isFirst = false;
   } else {
-    const [label, data] = event.data.split('|');
+    const [label, data] = [jsonData.time, jsonData.online];
     realtime.removeData();
     realtime.addData([Number(data)], label);
   }
